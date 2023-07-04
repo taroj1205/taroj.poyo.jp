c0b21b29b2fd59a6d36b66b8e88df7188c919610
taroj1205 <taroj1205@gmail.com>
2023-07-04 15:28:27 +1200

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 // The country to block from accessing the secret page
-const BLOCKED_COUNTRY = 'SE';
+const BLOCKED_COUNTRY = '';
 
 // Trigger this middleware to run on the `/secret-page` route
 export const config = {
@@ -17,7 +17,7 @@ export function middleware(request: NextRequest) {
 
     // Specify the correct route based on the requests location
     if (country === BLOCKED_COUNTRY) {
-        request.nextUrl.pathname = '/login';
+        request.nextUrl.pathname = '/';
     } else {
         request.nextUrl.pathname = `/chat`;
     }
