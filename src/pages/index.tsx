ddd71680de452e03ee4866e2281942665b48c647
taroj1205 <taroj1205@gmail.com>
2023-07-07 21:59:07 +1200

    translation in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import Profile from '../components/Profile';
+import { useTranslation } from 'react-i18next';
 
 const HomePage = () => {
+    const { t, i18n } = useTranslation('translation'); // Specify the translation key
+
+    useEffect(() => {
+        const userLanguage = window.navigator.language;
+        let language;
+
+        // Check if user language is Japanese
+        if (userLanguage.includes('ja')) {
+            language = 'ja'; // Set Japanese language
+        } else {
+            language = 'en'; // Set English as fallback language
+        }
+        i18n.changeLanguage(language);
+    }, []);
     return (
         <main className="container mx-auto py-10 max-w-6xl">
-            <h2 className="text-4xl">Welcome to my home page!</h2>
+            <h2 className="text-4xl">{t('index.welcome')}</h2>
             <Profile />
         </main>
     );
