10fc1249b387ca65b646f23fd54b9aee9db736e5
taroj1205 <61367823+taroj1205@users.noreply.github.com>
2023-07-09 18:40:40 +1200

    Update SchoolHistory.tsx

--- src/components/SchoolHistory.tsx
+++ src/components/SchoolHistory.tsx
@@ -9,7 +9,7 @@ export default function SchoolHistory() {
                 {t('about.education.schoolHistory')}
             </h2>
             <div className="overflow-x-auto w-full">
-                <table className="w-full table-auto rounded-lg overflow-hidden border-collapse text-left transition-shadow duration-300 shadow-md hover:shadow-lg">
+                <table className="w-full table-auto rounded-lg overflow-hidden border-collapse whitespace-nowrap transition-shadow duration-300 shadow-md hover:shadow-lg">
                     <thead>
                         <tr className="bg-teal-700 text-white">
                             <th className="py-2 px-4 rounded-tl-lg">
