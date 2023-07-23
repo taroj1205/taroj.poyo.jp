42fdb6914d83651a7ce4001be5e7dc741fa85f49
taroj1205 <taroj1205@gmail.com>
2023-07-23 18:41:47 +1200

    expandable menu for mobile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -69,14 +69,22 @@ const Profile = () => {
     }, []);
 
     const ChevronDown = ({ color }: { color: string }) => (
-        <>
-            <FaCog className="text-black dark:text-white" size={20} style={{ marginRight: -2 }} />
-            <Svg>
+        <span className='dark:text-white flex items-center'>
+            {user ? (
+                <img
+                    className="w-8 h-8 rounded-full"
+                    src={user.picture ?? ''}
+                    alt="Profile picture"
+                />
+            ) : (
+                <FaCog className="text-black dark:text-white" size={20} style={{ marginRight: -2 }} />
+            )}
+            <Svg className={`flex items-center ${isDropdownOpen ? 'transform rotate-180' : ''} transition-all duration-300`}>
                 <path
                     d="M8.292 10.293a1.009 1.009 0 0 0 0 1.419l2.939 2.965c.218.215.5.322.779.322s.556-.107.769-.322l2.93-2.955a1.01 1.01 0 0 0 0-1.419.987.987 0 0 0-1.406 0l-2.298 2.317-2.307-2.327a.99.99 0 0 0-1.406 0z"
                     fill={color} // Use the color prop for the fill color
                     fillRule="evenodd" />
-            </Svg></>
+            </Svg></span>
     );
 
     const Svg = (p: JSX.IntrinsicElements['svg']) => (
@@ -97,78 +105,70 @@ const Profile = () => {
                 className="focus:outline-none"
                 onClick={toggleDropdown}
             >
-                {user ? (
-                    <img
-                        className="w-8 h-8 m-1 rounded-full"
-                        src={user.picture ?? ''}
-                        alt="Profile picture"
-                    />
-                ) : (
-                    <span className='dark:text-white flex items-center'>
-                        {
-                            isDropdownOpen ? (
-                                <ChevronDown color={isDarkMode ? 'white' : 'black'} />
-                            ) : (
-                                <ChevronDown color="gray" />
-                            )}
-                    </span>
-                )}
+                {
+                    isDropdownOpen ? (
+                        <ChevronDown color={isDarkMode ? 'white' : 'black'} />
+                    ) : (
+                        <ChevronDown color="gray" />
+                    )}
             </button>
-            {isDropdownOpen && (
-                <div className="absolute top-8 right-2 mt-1 w-48 bg-white rounded-md shadow-lg">
-                    <div className="py-1">
-                        {!user && (
-                            <>
-                                <button
-                                    aria-label="login"
-                                    className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                    onClick={() => handleLink('/api/auth/login')}
-                                >
-                                    <FaSignInAlt className="mr-2 inline" />
-                                    {t('login')}
-                                </button>
-                                <button
-                                    aria-label="go to settings"
-                                    className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                    onClick={() => handleLink('/settings')}
-                                >
-                                    <FaCog className="mr-2 inline" />
-                                    {t('settings')}
-                                </button>
-                            </>
-                        )}
-                        {user && (
-                            <>
-                                <button
-                                    aria-label="go to profile"
-                                    className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                    onClick={() => handleLink('/profile')}
-                                >
-                                    <FaUser className="mr-2 inline" />
-                                    {t('profile')}
-                                </button>
-                                <button
-                                    aria-label="go to settings"
-                                    className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                    onClick={() => handleLink('/settings')}
-                                >
-                                    <FaCog className="mr-2 inline" />
-                                    {t('settings')}
-                                </button>
-                                <button
-                                    aria-label="logout"
-                                    className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                    onClick={() => handleLink('/api/auth/logout')}
-                                >
-                                    <FaSignOutAlt className="mr-2 inline" />
-                                    {t('logout')}
-                                </button>
-                            </>
-                        )}
+            {
+                isDropdownOpen && (
+                    <div className="absolute top-8 right-2 mt-1 w-48 bg-white rounded-md shadow-lg">
+                        <div className="py-1">
+                            {!user && (
+                                <>
+                                    <button
+                                        aria-label="login"
+                                        className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        onClick={() => handleLink('/api/auth/login')}
+                                    >
+                                        <FaSignInAlt className="mr-2 inline" />
+                                        {t('login')}
+                                    </button>
+                                    <button
+                                        aria-label="go to settings"
+                                        className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        onClick={() => handleLink('/settings')}
+                                    >
+                                        <FaCog className="mr-2 inline" />
+                                        {t('settings')}
+                                    </button>
+                                </>
+                            )}
+                            {user && (
+                                <>
+                                    <button
+                                        aria-label="go to profile"
+                                        className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        onClick={() => handleLink('/profile')}
+                                    >
+                                        <FaUser className="mr-2 inline" />
+                                        {t('profile')}
+                                    </button>
+                                    <button
+                                        aria-label="go to settings"
+                                        className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        onClick={() => handleLink('/settings')}
+                                    >
+                                        <FaCog className="mr-2 inline" />
+                                        {t('settings')}
+                                    </button>
+                                    <button
+                                        aria-label="logout"
+                                        className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        onClick={() => handleLink('/api/auth/logout')}
+                                    >
+                                        <FaSignOutAlt className="mr-2 inline" />
+                                        {t('logout')}
+                                    </button>
+                                </>
+                            )}
+                        </div>
                     </div>
-                </div>
-            )}
-        </div>
+                )
+            }
+        </div >
     );
 };
 
