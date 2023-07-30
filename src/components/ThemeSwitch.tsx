88b284f4e8e903dffb464e03b090be7749dbfe1b
taroj1205 <taroj1205@gmail.com>
2023-07-30 20:42:38 +1200

    Update ThemeSwitch.tsx

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -3,25 +3,29 @@ import React, { useState, useEffect } from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi';
 
 const ThemeSwitch: React.FC = () => {
-    const [theme, setTheme] = useState<'dark' | 'light'>('light');
+    const [theme, setTheme] = useState<'dark' | 'light'>();
     const router = useRouter();
 
     // Function to toggle theme
     const toggleTheme = () => {
         const newTheme = theme === 'dark' ? 'light' : 'dark';
         setTheme(newTheme);
-        document.documentElement.classList.remove(theme);
-        document.documentElement.classList.add(newTheme);
+        document.documentElement.classList.toggle('dark');
         localStorage.setItem('theme', newTheme);
     };
 
     // Load theme from local storage on initial mount
     useEffect(() => {
-        const storedTheme = document.documentElement.classList.contains('dark');
-        if (storedTheme) {
-            setTheme('dark');
+        if (localStorage.getItem('theme')) {
+            const storedTheme = localStorage.getItem('theme') as 'dark' | 'light';
+            setTheme(storedTheme);
         } else {
-            setTheme('light');
+            const storedTheme = document.documentElement.classList.contains('dark');
+            if (storedTheme) {
+                setTheme('dark');
+            } else {
+                setTheme('light');
+            }   
         }
     }, []);
 
