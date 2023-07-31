2288a975f00da58b431c61b1947f33a01522271e
taroj1205 <taroj1205@gmail.com>
2023-07-31 20:55:39 +1200

    Update ChangeProfile.tsx

--- src/components/ChangeProfile.tsx
+++ src/components/ChangeProfile.tsx
@@ -9,6 +9,12 @@ const ChangeProfile = () => {
     const [url, setURL] = useState('');
     const [popupOpen, setPopupOpen] = useState(false);
     const [error, setError] = useState('');
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+    if (!mounted) return null; // Prevent rendering on the server-side
 
     const togglePopup = () => {
         setPopupOpen(!popupOpen);
@@ -68,13 +74,6 @@ const ChangeProfile = () => {
         <>
             {user && user.picture && (
                 <div className="flex flex-col items-center">
-                    <div className="relative w-32 h-32 mb-4">
-                        <img
-                            src={user.picture.toString()}
-                            alt="Profile picture"
-                            className="rounded-full object-cover w-full h-full cursor-pointer"
-                        />
-                    </div>
                     <button
                         className="text-black dark:text-white px-4 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 focus:outline-none dark:bg-gray-800 dark:hover:bg-gray-700"
                         aria-label='change profile picture button'
@@ -84,8 +83,8 @@ const ChangeProfile = () => {
                     </button>
                     {popupOpen && (
                         <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50">
-                            <div className="w-96 bg-white p-4 rounded-lg shadow-lg dark:bg-gray-800">
-                                <div className="flex justify-end">
+                            <div className="w-96 max-w-[90%] bg-white p-2 rounded-lg shadow-lg dark:bg-gray-800">
+                                <div className="flex justify-end pr-1">
                                     <button
                                         className="text-gray-600 dark:text-white"
                                         aria-label='close'
@@ -94,31 +93,42 @@ const ChangeProfile = () => {
                                         X
                                     </button>
                                 </div>
-                                <h2 className="text-xl font-semibold mb-2">
-                                    {t('change.profile picture URL')}
-                                </h2>
-                                <form className="flex items-center" onSubmit={handleUrlSubmit}>
-                                    <input
-                                        type="text"
-                                        id="profile-picture-url"
-                                        defaultValue={user?.picture}
-                                        onChange={(e) => {
-                                            setURL(e.target.value);
-                                            setError('');
-                                        }}
-                                        className="flex-grow border-none rounded-lg shadow-sm text-black dark:text-white bg-gray-200  h-9 dark:bg-gray-900"
-                                    />
-                                    <button
-                                        type="submit"
-                                        aria-label='change'
-                                        className="ml-2 px-4 py-2 text-black dark:text-white bg-gray-200 rounded-lg hover:bg-gray-300 focus:outline-none focus:bg-gray-700 dark:hover:bg-gray-700 dark:bg-gray-900 h-9"
-                                    >
-                                        {t('change.set picture')}
-                                    </button>
-                                </form>
-                                {error && (
-                                    <p className="text-red-500 mt-2">{error}</p>
-                                )}
+                                <div className='flex'>
+                                    <div className="relative w-32 h-32 mb-4">
+                                        <img
+                                            src={user.picture.toString()}
+                                            alt="Profile picture"
+                                            className="rounded-full object-cover w-full h-full cursor-pointer"
+                                        />
+                                    </div>
+                                    <div className='flex flex-col ml-2'>
+                                        <h2 className="text-xl font-semibold mb-2">
+                                            {t('change.profile picture URL')}
+                                        </h2>
+                                        <form className="flex items-center flex-col" onSubmit={handleUrlSubmit}>
+                                            <input
+                                                type="text"
+                                                id="profile-picture-url"
+                                                defaultValue={user?.picture}
+                                                onChange={(e) => {
+                                                    setURL(e.target.value);
+                                                    setError('');
+                                                }}
+                                                className="flex-grow border-none rounded-lg shadow-sm text-black dark:text-white bg-gray-200  h-9 dark:bg-gray-900"
+                                            />
+                                            <button
+                                                type="submit"
+                                                aria-label='change'
+                                                className="w-full mt-2 text-black dark:text-white bg-gray-200 rounded-lg hover:bg-gray-300 focus:outline-none focus:bg-gray-700 dark:hover:bg-gray-700 dark:bg-gray-900 h-9"
+                                            >
+                                                {t('change.set picture')}
+                                            </button>
+                                        </form>
+                                        {error && (
+                                            <p className="text-red-500 mt-2">{error}</p>
+                                        )}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     )}
