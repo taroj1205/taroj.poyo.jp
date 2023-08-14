772f796a4ff2eddc8dedec72e8a0a05ef82ba0db
taroj1205 <taroj1205@gmail.com>
2023-08-14 22:34:51 +1200

    show github stats in apps page

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
 module.exports = {
     i18n: {
-        locales: ['default' ,'en', 'ja'],
+        locales: ['default', 'en', 'ja'],
         defaultLocale: 'default',
         localeDetection: true,
     },
     trailingSlash: true,
     compress: true,
     images: {
-        domains: ['gravatar.com'],
+        domains: ['gravatar.com', 'github-readme-stats.vercel.app'],
     },
-async redirects() {
-    return [
-        {
-            source: '/auth',
-            destination: '/auth/login',
-            permanent: true,
-        },
-        {
-            source: '/chat',
-            destination: '/apps/chat',
-            permanent: true,
-        },
-    ];
-}
+    async redirects() {
+        return [
+            {
+                source: '/auth',
+                destination: '/auth/login',
+                permanent: true,
+            },
+            {
+                source: '/chat',
+                destination: '/apps/chat',
+                permanent: true,
+            },
+        ];
+    },
 };
--- src/pages/apps.tsx
+++ src/pages/apps.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Link from 'next/link';
 import Head from 'next/head';
 import { FaComments } from 'react-icons/fa';
 import { IconType } from 'react-icons';
+import { useTheme } from 'next-themes';
 
 const RouteLink = ({ path, label, icon: Icon }: { path: string; label: string; icon: IconType }) => (
     <Link href={path} className="group flex flex-col items-center justify-center p-5 rounded-lg hover:bg-opacity-70 transition-colors shadow-md bg-white dark:bg-zinc-950 text-black dark:text-white">
@@ -22,6 +23,14 @@ const RouteLink = ({ path, label, icon: Icon }: { path: string; label: string; i
 
 const Apps = () => {
     const { t } = useTranslation();
+    const { theme } = useTheme();
+    const [githubStatsImage, setGithubStatsImage] = useState('');
+    const [githubLanguagesImage, setGithubLanguagesImage] = useState('');
+
+    useEffect(() => {
+        setGithubStatsImage(`https://github-readme-stats.vercel.app/api?username=taroj1205&show_icons=true&hide_border=true&&count_private=true&include_all_commits=true&theme=${theme}`);
+        setGithubLanguagesImage(`https://github-readme-stats.vercel.app/api/top-langs/?username=taroj1205&show_icons=true&hide_border=true&layout=compact&langs_count=10&theme=${theme}`);
+    }, [theme]);
 
     const routes = [
         { path: '/apps/chat', label: 'Chat', icon: FaComments },
@@ -65,10 +74,24 @@ const Apps = () => {
                             />
                         ))}
                     </div>
+                    <div className="py-4 flex flex-col items-center gap-2">
+                        <img
+                            height="180"
+                            width="auto"
+                            src={githubStatsImage}
+                            alt={'GitHub Stats'}
+                        />
+                        <img
+                            height="180"
+                            width="auto"
+                            src={githubLanguagesImage}
+                            alt={'GitHub Languages Stats'}
+                        />
+                    </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default Apps;
