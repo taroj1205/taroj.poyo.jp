3e293390a6d36ef09e1ad2023b41172593c47834
taroj1205 <taroj1205@gmail.com>
2023-08-18 23:42:25 +1200

    Update ThemeSwitch.tsx

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -9,11 +9,11 @@ const ThemeSwitch: React.FC = () => {
 
     useEffect(() => {
         setMounted(true);
-        setTheme(document.documentElement.classList.contains('dark') ? 'dark' : 'light');
     }, []);
 
     useEffect(() => {
         if (mounted) {
+            setTheme(document.documentElement.classList.contains('dark') ? 'dark' : 'light');
             setLoading(false);
         }
     }, [theme, mounted]);
@@ -23,23 +23,19 @@ const ThemeSwitch: React.FC = () => {
         setTheme(newTheme);
     };
 
-    if (loading) {
-        return (
-            <div className="rounded-full cursor-pointer md:mr-0 mr-2">
-                <svg className="animate-spin h-6 w-6 text-gray-500" viewBox="0 0 24 24"></svg>
-            </div>
-        );
-    }
-
     return (
         <div
             className='text-gray-500 md:mr-0 mr-2 cursor-pointer dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2'
             onClick={toggleTheme}
         >
-            {theme === 'dark' ? (
-                <FiMoon className="text-indigo-500 w-6 h-6" />
+            {loading ? (
+                <svg className="animate-spin h-6 w-6 text-gray-500" viewBox="0 0 24 24"></svg>
             ) : (
-                <FiSun className="text-yellow-500 w-6 h-6" />
+                theme === 'dark' ? (
+                    <FiMoon className="text-indigo-500 w-6 h-6" />
+                ) : (
+                    <FiSun className="text-yellow-500 w-6 h-6" />
+                )
             )}
         </div>
     );
