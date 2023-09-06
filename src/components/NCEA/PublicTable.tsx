b63f19bb7dd8c50bc5b4b0c0b764b32f854df59b
taroj1205 <taroj1205@gmail.com>
2023-09-07 09:28:18 +1200

    improve graph and table design, support different levels

--- src/components/NCEA/PublicTable.tsx
+++ src/components/NCEA/PublicTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 interface Subject {
@@ -12,32 +12,106 @@ interface Subject {
 
 const NceaTable: React.FC<{ subjects: Subject[] }> = ({ subjects }) => {
     const { t } = useTranslation();
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+
+    // Function to categorize subjects into levels
+    const categorizeSubjects = (subjects: Subject[]) => {
+        const categorizedLevels: { [key: string]: Subject[] } = {};
+
+        subjects.forEach((subject) => {
+            const level = subject.standardNumber.split('.')[0];
+            if (!categorizedLevels[level]) {
+                categorizedLevels[level] = [];
+            }
+            categorizedLevels[level].push(subject);
+        });
+
+        return categorizedLevels;
+    };
+
+    // Categorize the subjects into levels
+    const categorizedSubjects = categorizeSubjects(subjects);
+
+    // Calculate the total credits for each year level
+    const totalCredits: { [key: string]: number } = {};
+    Object.keys(categorizedSubjects).forEach((level) => {
+        totalCredits[level] = categorizedSubjects[level].reduce(
+            (acc, subject) => acc + parseInt(subject.credits),
+            0
+        );
+    });
+
+    if (loading) {
+        return null;
+    }
+
     return (
-        <div className="overflow-x-auto max-w-full">
+        <div className="overflow-x-auto max-w-full mb-4">
             <table className="w-fit table-auto rounded-lg overflow-hidden border-collapse whitespace-nowrap transition-shadow duration-300 shadow-md hover:shadow-lg text-base md:text-lg">
-                <thead>
-                    <tr className="bg-teal-700 text-white text-left">
-                        <th className="py-2 px-4 rounded-tl-lg">{t('ncea.subjects')}</th>
-                        <th className="py-2 px-4">{t('ncea.credits')}</th>
-                        <th className="py-2 px-4 rounded-tr-lg">{t('ncea.achievement')}</th>
-                    </tr>
-                </thead>
-                <tbody className="text-black dark:text-white">
-                    {subjects.map((subject, index) => (
-                        <tr
-                            key={index}
-                            className={
-                                subjects.indexOf(subject) % 2 === 0
-                                    ? 'bg-gray-100 dark:bg-gray-800'
-                                    : 'bg-gray-200 dark:bg-gray-900'
-                            }
-                        >
-                            <td className="py-2 px-3 text-left">{subject.subject}</td>
-                            <td className="py-2 px-3 text-right">{subject.credits}</td>
-                            <td className="py-2 px-3 text-left">{subject.achievement}</td>
-                        </tr>
+                {Object.keys(categorizedSubjects)
+                    .sort((a, b) => Number(b) - Number(a))
+                    .map((level) => (
+                        <React.Fragment key={level}>
+                            <tr className="bg-teal-700">
+                                <td className="py-2 px-4" colSpan={2}>
+                                    <strong>Level {level}</strong>
+                                </td>
+                                <td className="py-2 px-4" colSpan={2}>
+                                    <strong>Credits {totalCredits[level]}</strong>
+                                </td>
+                            </tr>
+                            {categorizedSubjects[level].map((subject, index) => {
+                                console.log('index', index);
+                                if (index > 0) {
+                                    console.log('categorizedSubjects', subject.subject, categorizedSubjects[level][index - 1].subject);
+                                }
+                                return (
+                                    <React.Fragment key={index}>
+                                        <tbody>
+                                            {index === 0 && (
+                                                <tr className="bg-gray-300 dark:bg-gray-700">
+                                                    <td className="py-2 px-4" colSpan={4}>
+                                                        <strong>{subject.subject}</strong>
+                                                    </td>
+                                                </tr>
+                                            )}
+                                            {index > 0 && subject.subject !== categorizedSubjects[level][index - 1].subject && (
+                                                <tr className="bg-gray-300 dark:bg-gray-700">
+                                                    <td className="py-2 px-4" colSpan={4}>
+                                                        <strong>{subject.subject}</strong>
+                                                    </td>
+                                                </tr>
+                                            )}
+                                            <tr
+                                                key={index}
+                                                className={
+                                                    index % 2 === 0
+                                                        ? 'bg-gray-100 dark:bg-gray-800'
+                                                        : 'bg-gray-200 dark:bg-gray-900'
+                                                }
+                                            >
+                                                <td className="py-2 px-3 text-left whitespace-pre-wrap">
+                                                    {subject.standardNumber}{' - '}
+                                                    {subject.name}
+                                                </td>
+                                                <td className="py-2 px-3 text-left lg:text-right">
+                                                    {subject.credits}
+                                                </td>
+                                                <td className="py-2 px-3 text-left">
+                                                    {subject.achievement}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </React.Fragment>
+                                )
+                            })}
+                        </React.Fragment>
                     ))}
-                </tbody>
             </table>
         </div>
     );
